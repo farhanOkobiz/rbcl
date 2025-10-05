07d11f6f78465b26c0a00f7d712e75083b0c38ad
farhanOkobiz <farhan.okobiz@gmail.com>
2025-10-05 13:07:40 +0600

    djfk

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,11 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "rbcl-production.up.railway.app"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "rbcl-production.up.railway.app",
+      },
+      {
+        protocol: "https",
+        hostname: "rbcl-client.vercel.app",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
       },
     ],
   },
